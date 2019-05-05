32c7e85e6fc67ffa6ff847f6a24d4cd2174a4083
cevatarmutlu <40899455+cevatarmutlu@users.noreply.github.com>
2019-05-05 18:22:17 +0300

    asd

---
+++ counting.js
@@ -0,0 +1,8 @@
+function slideDownSiraDiv() {
+    $("#siraDiv").slideDown(slDwn);
+    setTimeout(slideDownDegerDiv, slDwn + 200);
+}
+
+function slideDownDegerDiv() {
+    $("#degerDiv").slideDown(slDwn);
+}
